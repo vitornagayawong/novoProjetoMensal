8ecf7354a70a106da098af2c867b9b733b9d3955
Vitor Nagaya Wong <144039909+vitornagayawong@users.noreply.github.com>
2024-05-27 13:09:59 -0300

    projeto feito, só falta estilizar

--- tentativa4/js/events.js
+++ tentativa4/js/events.js
@@ -6,17 +6,17 @@ function Event(title, description, date, initialTime, finalTime, urgency, record
    this.finalTime = finalTime
    this.urgency = urgency
    this.records = records
-   this.id = generateId()     
+   this.id = generateId()
 }
 
 function generateId() {
    let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
-   let nextId = 0;   
+   let nextId = 0;
    // Encontrar o maior ID existente
    if (eventsList.length > 0) {
       const maxId = Math.max(...eventsList.map(event => event.id));
       nextId = maxId + 1;
-   }   
+   }
 
    return nextId;
 }
@@ -39,7 +39,7 @@ function appendListCards() {
 
    for (let i = 0; i < events.length; i++) {
       createEventCard(events[i]);
-   }     
+   }
 }
 
 var indexCard = 0
@@ -77,6 +77,10 @@ function createEventCard(event) {
    const records = document.createElement("p")
    records.textContent = `Lembretes: ${event.records}`
 
+   const identificao = document.createElement('p')
+   identificao.textContent = `id: ${event.id}`
+
+   card.appendChild(identificao)
    card.appendChild(btnEdit)
    card.appendChild(btnDelete)
    card.appendChild(title)
@@ -91,39 +95,93 @@ function createEventCard(event) {
    divPrincipal.appendChild(card)
 }
 
-function excludeEvent(eventId) {   
-   
-   let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
+function excludeEvent(eventId) {
 
-   let indexToFind = eventsList.findIndex( object => object.id === eventId)
-   
-   if(indexToFind !== -1) {
+   let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
+   let indexToFind = eventsList.findIndex(object => object.id === eventId)
+
+   if (indexToFind !== -1) {
       eventsList.splice(indexToFind, 1)
-      
+
       localStorage.setItem('eventsListabc', JSON.stringify(eventsList));
 
       const divPrincipal = document.querySelector(".lista-eventos")
       divPrincipal.innerHTML = ''
 
       appendListCards()
-   }     
+   }
 
 }
 
-function openModal() {
-   new bootstrap.Modal('#modalAberto').show();
-}
+// function openModal() {
+//    new bootstrap.Modal('#modalAberto').show();
+// }
 
 
-function editEvent (eventId) {
-   let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
+function editEvent(eventId) {
+   // let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
 
-   let indexToFind = eventsList.findIndex( object => object.id === eventId)
+   // let indexToFind = eventsList.findIndex(object => object.id === eventId)
 
    // console.log(`Você clicou no evento de índice ${indexToFind}`)
-   window.alert('Clicou em editar')
-   
+   // window.alert('Clicou em editar')
+
+   // const editForm = document.querySelector('.editForm');
+   // editForm.style.display = 'block';
+
+   // document.body.style.backgroundColor = "rgba(0, 0, 0, 0.2)"
+
+   let eventsList = JSON.parse(localStorage.getItem('eventsListabc')) || [];
+   let indexToFind = eventsList.findIndex(object => object.id === eventId);
+
+   const editForm = document.getElementById('editForm');
+   editForm.style.display = 'flex';
+
+   const editBtn = document.querySelector('.editBtn')
+   console.log(editBtn)
+
+   editBtn.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      // console.log('Clicou em submeter')
+
+      eventsList[indexToFind].title = document.getElementById('editTitle').value;
+      eventsList[indexToFind].description = document.getElementById('editDescription').value;
+      eventsList[indexToFind].date = document.getElementById('editDate').value;
+      eventsList[indexToFind].initialTime = document.getElementById('editInitialTime').value;
+      eventsList[indexToFind].finalTime = document.getElementById('editFinalTime').value;
+      // eventsList[indexToFind].urgency = document.getElementById('editForm_urgencia_id').value;
+
+      const highUrgency = document.querySelector('#editForm_urgencia_alta')
+      const midleUrgency = document.querySelector('#editForm_urgencia_media')
+      const lowUrgency = document.querySelector('#editForm_urgencia_baixa')
+
+      if(highUrgency.checked) {
+         eventsList[indexToFind].urgency = "Alta"
+      } else if(midleUrgency.checked) {
+         eventsList[indexToFind].urgency = "Média"
+      } else if(lowUrgency.checked){
+         eventsList[indexToFind].urgency = "Baixa"
+      }
+
+      eventsList[indexToFind].records = document.getElementById('editRecords').value;
+
+
+      localStorage.setItem('eventsListabc', JSON.stringify(eventsList));
+
+      const divPrincipal = document.querySelector(".lista-eventos")
+      divPrincipal.innerHTML = ''
+
+      editForm.style.display = 'none';
+
+      appendListCards()
+
+   })
 }
+
+
+
+
 
 
 
